c0d72400816e52a44e2ee1feb90d76ba94bb1ca1
Mohamed Soliman <32719498+muhahm-creator@users.noreply.github.com>
2019-10-19 02:41:01 +0200

    Create a React app and publish it to GitHub Pages

---
+++ src/robots.js
@@ -0,0 +1,63 @@
+export const robots = [
+    {
+        id : 1,
+        name : 'Muhammed',
+        username : 'mado',
+        email : 'mhahmed@gmail.com'
+    },
+    {
+        id : 2,
+        name : 'Andro',
+        username : 'Andy',
+        email : 'andro@gmail.com'
+    },
+    {
+        id : 3,
+        name : 'Ali',
+        username : 'Ali',
+        email : 'Ali@gmail.com'
+    },
+    {
+        id : 4,
+        name : 'Hanfy',
+        username : 'Hanfy',
+        email : 'hanfnas@gmail.com'
+    },
+    {
+        id : 5,
+        name : 'ahmed',
+        username : 'AYoo',
+        email : 'ahmed@gmail.com'
+    },
+    {
+        id : 6,
+        name : 'john',
+        username : 'Jooo',
+        email : 'john@gmail.com'
+    },
+    {
+        id : 7,
+        name : 'Adham',
+        username : 'Adaaa',
+        email : 'Adhma@gmail.com'
+    },
+    {
+        id : 8,
+        name : 'youssef',
+        username : 'Yooo',
+        email : 'yousef@gmail.com'
+    },
+    {
+        id : 9,
+        name : 'Andrei',
+        username : 'Andooo',
+        email : 'andrei@gmail.com'
+    },
+    {
+        id : 10,
+        name : 'Brad',
+        username : 'Traversy',
+        email : 'brad@gmail.com'
+    }
+
+];
