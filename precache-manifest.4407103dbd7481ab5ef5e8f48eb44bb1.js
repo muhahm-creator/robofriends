7892e16fa23e3ab6757bf376b9b1c7d787b2c9d9
Mohamed Soliman <32719498+muhahm-creator@users.noreply.github.com>
2019-10-19 02:38:56 +0200

    Updates

---
+++ precache-manifest.4407103dbd7481ab5ef5e8f48eb44bb1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7806620ad76a81b154725bfb6cfc670f",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "07e86aa12f6794c51d81",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "76ebb69d516c17ed3609",
+    "url": "/robofriends/static/css/main.7c8bf876.chunk.css"
+  },
+  {
+    "revision": "07e86aa12f6794c51d81",
+    "url": "/robofriends/static/js/2.e6eb871a.chunk.js"
+  },
+  {
+    "revision": "76ebb69d516c17ed3609",
+    "url": "/robofriends/static/js/main.d743eaac.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
